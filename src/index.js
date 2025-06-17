3c0693571afe4fec56fb355e873be82854db6558
Abel Gasque <abelgasque20@gmail.com>
2025-06-17 19:44:40 -0300

    Add packege cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import "dotenv/config";
 import app from './configs/http-server.js';
+import cors from 'cors';
 import { connectToDatabase } from "./database/connect.js";
 
 const port = process.env.NODE_PORT || 9090;
 
 await connectToDatabase();
 
-app().listen(port, () => {
+const server = app();
+
+server.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+server.listen(port, () => {
     if (process.env.NODE_ENV === "development") {
         console.log(`Aplicação executando em: http://localhost:${port}`);
     }
