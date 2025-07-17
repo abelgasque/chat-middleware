84ed0fc3de9e9bfdcb07876769fed817f1a0eec3
Abel Gasque <abelgasque20@gmail.com>
2025-07-17 16:06:39 -0300

    Create connection in tenant

--- src/api/services/tenant.service.js
+++ src/api/services/tenant.service.js
@@ -1,37 +1,12 @@
 import { Tenant } from "../../database/models/index.js";
+import { getTenantSequelize } from "../../database/connect-tenant.js";
 import NotFoundRequestException from "../utils/not-found-request.exception.js";
-import { Sequelize } from "sequelize";
-
-const {
-    DB_SERVER,
-    DB_PORT,
-    DB_DATABASE,
-    DB_USERNAME,
-    DB_PASSWORD,
-} = process.env;
 
 class TenantsService {
     async create(data) {
-        // const tenant = await Tenant.create(data);
 
-        const databaseName = data.database;
-        const sequelize = new Sequelize(
-            DB_DATABASE,
-            DB_USERNAME,
-            DB_PASSWORD,
-            {
-                host: DB_SERVER,
-                port: DB_PORT,
-                dialect: "postgres",
-                logging: false,
-            }
-        );
-
-        try {
-            await sequelize.query(`CREATE DATABASE IF NOT EXISTS \`${databaseName}\`;`);
-        } finally {
-            await sequelize.close();
-        }
+        const tenant = await Tenant.create(data);
+        getTenantSequelize(data.database);
 
         return tenant;
     }
